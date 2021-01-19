2283ff1aa64fe5d13823d44135444c7f786aa99c
Jude kuti <judekuti@gmail.com>
2021-01-19 13:04:25 -0600

    Create elements based on html collections

--- app.js
+++ app.js
@@ -22,10 +22,17 @@ const elements = {
 
 const create = ({ label, type, name, id }) => `<label for=${label}>${label}</label></br><input type=${type} name=${name} id=${id} />`;
 
-let _s = (val) => document.querySelector(`#${val}`);
+let _s = val => document.querySelector(`#${val}`);
+let s_ = val => document.querySelectorAll(`.${val}`);
 
+// for creating elements based on single id
 const createEl = (ele) => {
     _s(ele).addEventListener('click', () => _s("el").innerHTML = create(elements[ele]));
 }
-
 createEl("text");
+
+// for creating elements based on html collections
+const createMultiEl = ele => _s("el").innerHTML = create(elements[ele]);
+s_('btn-el').forEach(x => x.addEventListener('click', function() {
+    createMultiEl(this.id)
+}));
